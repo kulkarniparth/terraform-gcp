52a76a3ca481c9b57253973030b3a0c2b03a00fc
Parth Kulkarni <parthkulkarni998@gmail.com>
2023-02-11 11:47:06 +0000

    destroy bucket

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "bucket" {
-  name     = var.bucket_name
-  location = var.location
-  project  =  var.project_id
-}
+# resource "google_storage_bucket" "bucket" {
+#   name     = var.bucket_name
+#   location = var.location
+#   project  =  var.project_id
+# }
