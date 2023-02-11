350f56965a7d3cd709d205eff9e3bb230d9e70e2
Parth Kulkarni <parthkulkarni998@gmail.com>
2023-02-11 14:07:07 +0000

    object delete event

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "google_storage_notification" "notification" {
   bucket         = var.bucket_name
   payload_format = "JSON_API_V1"
   topic          = var.topic_name
-  event_types    = ["OBJECT_FINALIZE", "OBJECT_METADATA_UPDATE"]
+  event_types    = ["OBJECT_FINALIZE", "OBJECT_METADATA_UPDATE","OBJECT_DELETE"]
   custom_attributes = {
     new-attribute = "new-attribute-value"
   }
